1e30a20254f297493da350f5fa8b28f1048ba62e
Scott Lin <scotttlin@gmail.com>
2016-04-26 19:57:28 -0700

    Add anaconda-mode

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -2,4 +2,20 @@
   :mode (("\\.py\\'" . python-mode)
          ("SConstruct\\'" . python-mode)
          ("SConscript\\'" . python-mode))
-  :interpreter ("python" . python-mode))
+  :interpreter ("python" . python-mode)
+  :config
+  (use-package anaconda-mode
+    :ensure t
+    :defer t
+    :commands anaconda-mode
+    :diminish anaconda-mode
+    :config
+    (progn
+      (add-hook 'python-mode-hook 'anaconda-mode)
+      (add-hook 'python-mode-hook 'eldoc-mode)))
+
+  (use-package company-anaconda
+    :ensure t
+    :defer t
+    :init
+    (after company-mode (add-to-list 'company-backends 'company-anaconda))))
