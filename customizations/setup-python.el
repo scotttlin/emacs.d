c5903592ff96f0ff5cc86b5a978ec1f3cb656d52
Scott Lin <scotttlin@gmail.com>
2016-04-26 18:38:00 -0700

    Removing all the files in gitignore

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -2,8 +2,4 @@
   :mode (("\\.py\\'" . python-mode)
          ("SConstruct\\'" . python-mode)
          ("SConscript\\'" . python-mode))
-  :interpreter ("python" . python-mode)
-  :config
-  (use-package py-autopep8
-    :config
-    (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)))
+  :interpreter ("python" . python-mode))
